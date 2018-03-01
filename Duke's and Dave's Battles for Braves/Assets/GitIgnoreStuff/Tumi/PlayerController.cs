1c4f3cba22f05648fdb7ae2650beab311f81d974
TumiDesktop <tumi435@gmail.com>
2018-03-01 02:36:45 +0000

    Bætti við test scenu, player control og kameru sem svona tegist til þegar playes hreyfa sig í burtu frá hvorum öðrum.

---
+++ Duke's and Dave's Battles for Braves/Assets/GitIgnoreStuff/Tumi/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public string forwardKey;
+    public string backwardsKey;
+    public string leftKey;
+    public string rightKey;
+
+    // Use this for initialization
+    void Start () {
+        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void PlayerInput()
+    {
+        if (Input.GetKey(rightKey))
+        {
+            rb.AddForce(speed, 0, 0, ForceMode.Impulse);
+        }
+        if (Input.GetKey(leftKey))
+        {
+            rb.AddForce(-speed, 0, 0, ForceMode.Impulse);
+        }
+        if (Input.GetKey(forwardKey))
+        {
+            rb.AddForce(0, 0, speed, ForceMode.Impulse);
+        }
+        if (Input.GetKey(backwardsKey))
+        {
+            rb.AddForce(0, 0, -speed, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        PlayerInput();
+    }
+}
