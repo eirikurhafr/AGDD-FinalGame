f8b3e15e5bf1b4552b064ffc48b64cabcfbec9f4
hallur14 <hallur14@ru.is>
2018-04-04 21:03:41 +0000

    menu stuff and fixes

--- Duke's and Dave's Battles for Braves/Assets/Scripts/Menu/StartMenu.cs
+++ Duke's and Dave's Battles for Braves/Assets/Scripts/Menu/StartMenu.cs
@@ -6,9 +6,34 @@ using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour {
 
+	public GameObject ControlsImage;
+	public GameObject CloseControlsButton;
+	private bool flip = true;
+
+	void Start()
+	{
+		ControlsImage.SetActive(false);
+		CloseControlsButton.SetActive(false);
+	}
+
 	public void StartGame()
 	{
 		SceneManager.LoadScene("NewLevelScene");
+	}
+
+	public void Controls()
+	{
+		ControlsImage.SetActive(flip);
+		CloseControlsButton.SetActive(flip);
+		flip = !flip;
+		
+	}
+
+	public void CloseControls()
+	{
+		ControlsImage.SetActive(false);
+		CloseControlsButton.SetActive(false);
+		flip = true;
 	}
 
 	public void ExitGame()
