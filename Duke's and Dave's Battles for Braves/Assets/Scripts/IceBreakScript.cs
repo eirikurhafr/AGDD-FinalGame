e260db084c419172f432f84ae2051486e8eeab9c
agustha16 <agustha16@ru.is>
2018-04-04 23:44:52 +0000

    fixed gate and frost level

--- Duke's and Dave's Battles for Braves/Assets/Scripts/IceBreakScript.cs
+++ Duke's and Dave's Battles for Braves/Assets/Scripts/IceBreakScript.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class IceBreakScript : MonoBehaviour {
 
     public GameObject scroll;
+    public GameObject spawnObject;
+    private float timer;
+    private float counter;
+    private bool spawnerCheck;
 
     private void Start()
     {
         scroll.gameObject.SetActive(false);
+        timer = 20f;
+        counter = 0;
+        spawnerCheck = true;
     }
 
     public void explosionFunction()
     {
         scroll.gameObject.SetActive(true);
         Destroy(gameObject);
+    }
+    
+
+    private void Update()
+    {
+        counter += Time.deltaTime;
+        if(counter >= timer)
+        {
+            counter = 0;
+            spawnerCheck = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+{
+        if ((other.gameObject.name == "Player_1" || other.gameObject.name == "Player_2") && spawnerCheck)
+        {
+            Instantiate(spawnObject, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.identity);
+            spawnerCheck = false;
+        }
     }
 }
---
+++ Duke's and Dave's Battles for Braves/Assets/Scripts/ScrollScriptEnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScriptEnd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.GetComponent<BoxCollider>() == null)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+	}
+}
